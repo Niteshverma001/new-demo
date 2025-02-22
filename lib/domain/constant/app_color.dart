0c376c41beacd46a906d2a497cc59def65d42db4
nitesh verma <niteshverma7376@gmail.com>
2025-02-22 23:53:47 +0530

    first change

---
+++ lib/domain/constant/app_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color Scaffoldbackground=Color(0XFFF7CB45);
+}
---
+++ lib/repository/screens/splash/splashscreen.dart
@@ -0,0 +1,36 @@
+// import 'package:flutter/cupertino.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:project1/domain/constant/app_color.dart';
+import 'package:project1/repository/screens/login/loginscreen.dart';
+import 'package:project1/repository/widgets/uihelper.dart';
+
+class SplashScreen extends StatefulWidget{
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.Scaffoldbackground,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Uihelper.CustomImage(img: "laundry-wash.png"),
+        ],),
+      ),
+
+    );
+  }
+}
